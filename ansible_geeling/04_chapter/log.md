12b0ed87806f765fbd55a40ea1868e5bba18f596
BansheePrime <cmapuk-xpehob@yandex.ru>
2024-02-15 12:36:23 +0300

    updating latest work

---
+++ ansible_geeling/04_chapter/log.md
@@ -0,0 +1,5 @@
+## yet to be done
+Friday - reverse proxy 33  
+Robinson - app server 34  
+
+### 
---
+++ image/dotfiles/vagrant_autocomp.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+vagrant autocomplete install --bash
+#vagrant autocomplete install --zsh
