7eae65b795e455f0931e48824dfa2ba75fcb8ecc
BansheePrime <cmapuk-xpehob@yandex.ru>
2024-02-19 15:12:14 +0300

    changing layout

---
+++ ansible_geeling/robinson-friday_configuration/app/stopwatch01.py
@@ -0,0 +1,52 @@
+from textual.app import App, ComposeResult
+from textual.containers import ScrollableContainer
+from textual.widgets import Button, Header, Footer, Static
+
+
+
+class TimeDisplay(Static):
+    """A widget to display elapsed time."""
+
+
+class Stopwatch(Static):
+    """A stopwatch widget."""
+
+    def on_button_pressed(self, event: Button.Pressed) -> None:
+        """Event handler called when a button is pressed."""
+        if event.button.id == "start":
+            self.add_class("started")
+        elif event.button.id == "stop":
+            self.remove_class("started")
+
+    def compose(self) -> ComposeResult:
+        """Create child widgets of a stopwatch."""
+        yield Button("Start", id="start")
+        yield Button("Stop", id="stop")
+        yield Button("Reset", id="reset")
+        yield TimeDisplay("00:00:00.00")
+
+
+
+class StopwatchApp(App):
+    """A Textual app to manage stopwatches."""
+
+    CSS_PATH = "./stopwatch03.tcss"
+    BINDINGS = [("d", "toggle_dark", "Toggle dark mode")]
+
+    def compose(self) -> ComposeResult:
+        """Create child widgets for the app."""
+
+        yield Header()
+        yield Footer()
+        yield ScrollableContainer(Stopwatch(), Stopwatch(), Stopwatch())
+
+    def action_toggle_dark(self) -> None:
+        """An action to toggle dark mode."""
+        self.dark = not self.dark
+
+
+
+if __name__ == "__main__":
+    app = StopwatchApp()
+    app.run()
+
--- ansible_geeling/robinson-friday_configuration/log.md
+++ ansible_geeling/robinson-friday_configuration/log.md
@@ -2,4 +2,6 @@
 Friday - reverse proxy 33  
 Robinson - app server 34  
 
-### 
+### adding textual_dev env and ignoring it
+
+
